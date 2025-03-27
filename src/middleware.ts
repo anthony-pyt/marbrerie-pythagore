1f107ae6faacccfcdb254caaea941b834deaf317
anthony-pyt <anthony@marbrerie-pythagore.fr>
2025-03-27 13:57:55 +0100

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get("pythagore_gateway_session")?.value;
-
-  console.log(req.nextUrl.pathname);
   
   // Si l'utilisateur est déjà connecté et tente d'accéder à /login
   if (req.nextUrl.pathname === "/login" && token) {
