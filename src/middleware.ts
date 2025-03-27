a1b784d5eab5664c7a1abdff788b4e6f345fe10a
anthony-pyt <anthony@marbrerie-pythagore.fr>
2025-03-27 12:24:20 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  
   const token = req.cookies.get("pythagore_gateway_session")?.value;
 
-  if (!token) {
+  console.log(req.nextUrl.pathname);
+  
+  // Si l'utilisateur est déjà connecté et tente d'accéder à /login
+  if (req.nextUrl.pathname === "/login" && token) {
+    return NextResponse.redirect(new URL("/admin", req.url));
+  }
+
+  // Si l'utilisateur n'est pas connecté et tente d'accéder à /admin
+  if (!token && req.nextUrl.pathname.startsWith("/admin")) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
@@ -12,5 +19,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: "/admin/:path*",
+  matcher: ["/admin/:path*", "/login"],
 };
