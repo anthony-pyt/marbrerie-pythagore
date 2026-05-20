873535fe478e13ce99852e518e22b02ca3cff0f2
anthony-pyt <anthony@marbrerie-pythagore.fr>
2026-05-20 12:08:42 +0200

    Update route.js

--- src/app/api/stock/patterns/route.js
+++ src/app/api/stock/patterns/route.js
@@ -11,7 +11,7 @@ export async function GET(request) {
     return Response.json(cached.data);
   }
 
-  const apiUrl = process.env.API_GATEWNEXT_PUBLIC_API_STOCK_URLAY_URL;
+  const apiUrl = process.env.NEXT_PUBLIC_API_STOCK_URL;
   try {
     const response = await axios.get(`${apiUrl}/patterns`);
 
